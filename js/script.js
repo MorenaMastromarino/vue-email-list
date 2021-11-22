fc7d88e9906af6614eeea93a0e1224090a638e19
MorenaMastromarino <morena.mastromarino98@gmail.com>
2021-11-22 17:43:35 +0100

    button per generare nuove mail

--- js/script.js
+++ js/script.js
@@ -9,8 +9,12 @@ const app = new Vue({
 
   methods:{
     getMailList(){
+      this.mailList = [];
+      this.isLoading = true;
+      this.isError = false;
+
       for(let i=0; i < 10; i++){
-        axios.get('https://flynn.boolean.careers/exercises/api/random/maill')
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
 
           .then((response) =>{
             const data = response.data;
